5d64168c0d06acd76d9c12ca92e66595af228384
professor-fennel <62268736+professor-fennel@users.noreply.github.com>
2023-01-25 20:15:32 -0600

    game loop and mouse input

--- quantum-aces.js
+++ quantum-aces.js
@@ -1,9 +1,19 @@
-function setup() {
+game = new GameState();
 
+function setup() {
+  colorMode(RGB, 255, 255, 255, 1);
+  w = 480;
+  h = w;
+  var canvas = createCanvas(w, h);
+  canvas.parent('script-box');
 }
 
 function draw() {
-
+  noStroke();
+  fill(0, 0, 0, 0.1);
+  rect(-10, -10, w + 10, h + 10);
+  fill(40, 200, 40, 1);
+  ellipse(mouseX, mouseY, 20, 20);
 }
 
 class Ship {
@@ -13,7 +23,13 @@ class Ship {
 }
 
 class GameState {
+  constructor() {
+    this.state = "GAME_START";
+  }
 
+  checkState() {
+
+  }
 }
 
 class Vector {
@@ -22,7 +38,8 @@ class Vector {
     this.y = y;
   }
 
-  function add(v) {
-    
+  add(v) {
+    added = new Vector(this.x + v.x, this.y + v.y);
+    return added
   }
 }
